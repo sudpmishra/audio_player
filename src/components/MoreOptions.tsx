2e2419c796b684bd15fa95067f88a327d9ef385b
Sudeep Mishra <sudeep014@gmail.com>
2025-02-03 15:45:12 +0545

    Refactor AudioPlayer and MoreOptions components: update props to use track object, enhance state management for favorites, and improve code readability

--- src/components/MoreOptions.tsx
+++ src/components/MoreOptions.tsx
@@ -1,37 +1,33 @@
+import { Track } from "@/tracks";
 import { useEffect, useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa6";
 import { FiMoreHorizontal } from "react-icons/fi";
 
 type MoreOptionsProps = {
-    trackId: string;
+    track: Track;
     favouriteTrack: (trackId: string) => void;
     setShowLyrics: (showLyrics: boolean) => void;
     showLyrics: boolean;
 };
 
-const MoreOptions: React.FC<{
-    trackId: string;
-    favouriteTrack: (trackId: string) => void;
-    setShowLyrics: (showLyrics: boolean) => void;
-    showLyrics: boolean;
-}> = ({ trackId, favouriteTrack, setShowLyrics, showLyrics }) => {
+const MoreOptions: React.FC<MoreOptionsProps> = ({ track, favouriteTrack, setShowLyrics, showLyrics }) => {
     const [isFavourite, setIsFavorite] = useState(false);
     useEffect(() => {
         const favouriteTracks = JSON.parse(
             localStorage.getItem("favouriteTracks") || "[]"
         );
-        setIsFavorite(favouriteTracks.includes(trackId));
-    }, [trackId]);
+        setIsFavorite(favouriteTracks.includes(track.id));
+    }, [track.id]);
     return (
         <div className="p-4 text-gray-400 grid grid-rows-2 gap-4">
             <FiMoreHorizontal className="w-5 h-5 cursor-pointer justify-self-end" onClick={() => setShowLyrics(!showLyrics)} />
             <div className="flex items-center gap-2 justify-self-end">
-                <span className="text-sm">854K</span>
+                <span className="text-sm">{track.likes}</span>
                 {isFavourite ? (
                     <FaHeart
                         className="w-5 h-5 cursor-pointer text-red-500 animate-[ping_.3s_ease-in-out]"
                         onClick={() => {
-                            favouriteTrack(trackId);
+                            favouriteTrack(track.id);
                             setIsFavorite(false);
                         }}
                     />
@@ -39,7 +35,7 @@ const MoreOptions: React.FC<{
                     <FaRegHeart
                         className="w-5 h-5 cursor-pointer"
                         onClick={() => {
-                            favouriteTrack(trackId);
+                            favouriteTrack(track.id);
                             setIsFavorite(true);
                         }}
                     />
